d8d249d33a2a8ea77d6349b39a3e854528400629
Utkarsh Sinha <utkarsh2019p@gmail.com>
2024-09-07 17:09:59 +0530

    Responsive-1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import ProductCard from "./ProductCard";
 
-const Home = ({loading, products}) => {
-
+const Home = ({ loading, products }) => {
   return (
     <div className="flex min-h-[80vh] justify-center pb-6">
       {loading ? (
@@ -12,9 +11,9 @@ const Home = ({loading, products}) => {
       ) : products.length === 0 ? (
         <div>No Products Found</div>
       ) : (
-        <div className="w-[75vw] mt-6 flex flex-wrap justify-center gap-3 md:w-[60vw]">
+        <div className="w-[75vw] mt-6 flex flex-wrap justify-center gap-5 lg:gap-3 md:w-[60vw]">
           {products.map((product) => (
-            <ProductCard key={product.id} product={product}/>
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
       )}
